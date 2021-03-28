35e560a8ef862379349f6bec8320ea8bd016ec5a
IzMichael <themichael77@outlook.com>
2021-03-29 08:07:27 +1300

    Build Site

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    const pages = ["1500", "1600", "1700", "1800", "1900", "2000"];
+    pages.forEach(page => {
+        fetch('/texts/' + page + '.md')
+        .then(data => data.text())
+        .then(response => print(page, response))
+    });
+};
+
+function print(id, text) {
+    document.getElementById(id).innerHTML = text;
+}
